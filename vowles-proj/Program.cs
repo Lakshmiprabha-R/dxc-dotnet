2aecf3d3924a4e86fd49799f7a59c795f489c013
Rajendran <l.rajendran@dxc.com>
2023-04-20 13:38:32 +0530

    Initial Commit

---
+++ vowles-proj/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.ComponentModel.Design;
+
+Console.WriteLine("Enter the character");
+char ap = Convert.ToChar(Console.ReadLine());
+if (ap == 'A' || ap == 'E' || ap == 'I' || ap == 'O' || ap == 'U') 
+{
+    Console.WriteLine("It's an vowel"); 
+}
+
+else
+{
+    Console.WriteLine("It's not an vowel");
+}
+
