79fcdf142783b52986336148540be791d3a17e1f
Rajendran <l.rajendran@dxc.com>
2023-04-21 10:58:44 +0530

    Initial Commit

---
+++ iterative2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+    Console.WriteLine("Temperature in Celsius\t Temperature in Fahrenheit");
+    for (int celsius = 0; celsius <= 100; celsius += 10)
+    {
+        double fahrenheit = (celsius * 9 / 5.0) + 32;
+        Console.WriteLine($"{celsius}C\t\t{fahrenheit}F");
+    }
+}
+
+
