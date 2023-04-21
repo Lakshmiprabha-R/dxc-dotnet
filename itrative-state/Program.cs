d885f98c1b95e87e9bcdb594f3df33a8d7873476
Rajendran <l.rajendran@dxc.com>
2023-04-21 10:39:11 +0530

    Initial Commit

---
+++ itrative-state/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+{
+
+    int n, sum = 0, m;
+    Console.WriteLine("Enter numbers:");
+    n = int.Parse(Console.ReadLine());
+    while (n > 0)
+    {
+        m = n % 10;
+        sum = sum + m;
+        n = n / 10;
+    }
+    Console.WriteLine("Sum is = " + sum);
+}
